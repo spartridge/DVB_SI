ddba3f91b4d131c40a26ab840bf91bbccbf0ba42
Stan Partridge <Stan.Partridge@arrisi.com>
2015-02-24 12:30:07 -0500

    Update source files with LGPLv2 header.

--- sistorage/src/dvbdb.cpp
+++ sistorage/src/dvbdb.cpp
@@ -1,9 +1,22 @@
-// Copyright (c) 2014 ARRIS Group, Inc. All rights reserved.
 //
-// This program is confidential and proprietary to ARRIS Group, Inc. (ARRIS),
-// and may not be copied, reproduced, modified, disclosed to others, published
-// or used, in whole or in part, without the express prior written permission
-// of ARRIS.
+// DVB_SI for Reference Design Kit (RDK)
+//
+// Copyright (C) 2015  Dmitry Barablin & Stan Partridge (stan.partridge@arris.com)
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 #include "dvbdb.h"
 
